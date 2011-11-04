c914ab9e5402f3ce4a25ee6b4053aa934197f796
Josh Reed <obitus9982@gmail.com>
2011-11-03 23:59:54 -0500

    Flesh out the create() method and tests

--- grails-app/controllers/countable/BucketController.groovy
+++ grails-app/controllers/countable/BucketController.groovy
@@ -2,7 +2,10 @@ package countable
 
 import grails.converters.JSON
 
+import java.security.MessageDigest
+
 class BucketController {
+	private static final String COLLECTION = "buckets"
 	static allowedMethods = [post: 'POST', create: 'POST']
 
 	def mongoService
@@ -18,7 +21,35 @@ class BucketController {
 	}
 
 	def create() {
-		render "Create: ${params.bucket}"
+		// validate the request
+		def errors = [:]
+		if (!params.bucket) { errors.bucket = 'A bucket name is required' }
+		if (!params.email) { errors.email = 'An email address is required' }
+
+		def bucket = getBucket(params.bucket)
+		if (bucket) { errors.bucket = 'That bucket is already registered' }
+
+		if (errors) {
+			render view: '/index', model: [bucket: params.bucket, email: params.email, errors: errors]
+			return
+		}
+
+		// create the bucket
+		bucket = [
+			'name': params.bucket,
+			'email': params.email,
+			'token': createToken(params.bucket),
+			'enabled': true,
+			'activated': false,
+			'public': false,
+			'created': new Date()
+		]
+		buckets.add(bucket)
+
+		// TODO: send the email
+
+		flash.message = "Your bucket has been registered! Check your email for instructions on how to activate and start counting things"
+		redirect uri:"/"
 	}
 
 	/* admin services */
@@ -37,11 +68,17 @@ class BucketController {
 		render "${params.secret}: Post"
 	}
 
-	private getBucket(bucket) {
-		if (bucket) {
-			mongoService.getCollection('buckets', true).find(name: bucket)
-		} else {
-			null
-		}
+	private getBuckets() {
+		mongoService.getCollection(COLLECTION, true)
+	}
+
+	private getBucket(name) {
+		name ? buckets.find(name: name) : null
+	}
+
+	private createToken(bucket) {
+		MessageDigest md = MessageDigest.getInstance('SHA')
+		md.update("${bucket}${new Date().time}".getBytes('UTF-8'))
+		return md.digest().encodeAsSHA1()
 	}
 }
--- test/unit/countable/BucketControllerTests.groovy
+++ test/unit/countable/BucketControllerTests.groovy
@@ -66,9 +66,41 @@ class BucketControllerTests {
 	}
 
 	void testCreate() {
+		controller.params.putAll(bucket: 'refactr', email: 'test@example.com')
+		controller.request.method = "POST"
+		controller.create()
+		assert 'Your bucket has been registered! Check your email for instructions on how to activate and start counting things' == controller.flash.message
+		assert '/' == controller.response.redirectedUrl
+	}
+
+	void testCreateRequiresBucket() {
+		controller.params.putAll(email: 'test@example.com')
+		controller.request.method = "POST"
+		controller.create()
+		assert '/index' == view
+		assert [bucket: null, email: 'test@example.com', errors: [bucket: 'A bucket name is required']] == model
+	}
+
+	void testCreateRequiresEmail() {
+		controller.params.putAll(bucket: 'refactr')
+		controller.request.method = "POST"
+		controller.create()
+		assert '/index' == view
+		assert [bucket: 'refactr', email: null, errors: [email: 'An email address is required']] == model
+	}
+
+	void testCreateRequiresBucketAndEmail() {
+		controller.request.method = "POST"
+		controller.create()
+		assert '/index' == view
+		assert [bucket: null, email: null, errors: [bucket: 'A bucket name is required', email: 'An email address is required']] == model
+	}
+
+	void testCreateAlreadyExists() {
 		controller.params.putAll(bucket: 'test', email: 'test@example.com')
 		controller.request.method = "POST"
 		controller.create()
-		assert 'Create: test' == controller.response.contentAsString
+		assert '/index' == view
+		assert [bucket: 'test', email: 'test@example.com', errors: [bucket: 'That bucket is already registered']] == model
 	}
 }
